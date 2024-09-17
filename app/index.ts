76b39cd1da652e26e5da5b91afb92468bf9ddd9a
Michael <jluz@gbox.adnu.edu.ph>
2024-09-18 02:57:27 +0800

    [Modification] - Trying to make vercel work

--- app/index.ts
+++ app/index.ts
@@ -3,6 +3,7 @@ import { Hono } from "hono";
 import axios from "axios";
 import { recipesRoute } from "./modules/recipes/recipes.route";
 import { cors } from "hono/cors";
+import { handle } from "hono/vercel";
 
 const app = new Hono();
 
@@ -17,7 +18,10 @@ app.route("/recipes", recipesRoute);
 const port = 3100;
 console.log(`Server is running on port ${port}`);
 
-serve({
-  fetch: app.fetch,
-  port,
-});
+const handler = handle(app);
+
+export const GET = handler;
+export const POST = handler;
+export const PATCH = handler;
+export const PUT = handler;
+export const OPTIONS = handler;
